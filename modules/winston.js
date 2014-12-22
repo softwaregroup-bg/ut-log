044d11abdb223bd1d1d99a3793d6a40be10d67a3
stamen.peev <stamen.peev@softwaregroup-bg.com>
2014-12-22 11:17:19 +0200

    add custom logic when logging with winston

--- modules/winston.js
+++ modules/winston.js
@@ -1,4 +1,5 @@
 (function(define) {define(function(require) {
+    var util        = require('util');
     var winston     = require('winston');
     var levels      = {trace:10, debug:20, info:30, warn:40, error:50, fatal:60};
     var container   = new winston.Container();
@@ -23,13 +24,35 @@
                     meta.name = name;
                     return meta;
                 });
+                function logHandler(level, data) {
+                    if (typeof data[0] !== 'string') {
+                        try { // stringify if object literal
+                            data[0] = JSON.stringify(data[0], null, 2);
+                        } catch (e) {// inspect if complex structure
+                            data[0] = util.inspect(data[0]);
+                        }
+                    }
+                    log[level].apply(log, data);
+                }
                 return {
-                    trace   :   log.trace.bind(log),
-                    debug   :   log.debug.bind(log),
-                    info    :   log.info.bind(log),
-                    warn    :   log.warn.bind(log),
-                    error   :   log.error.bind(log),
-                    fatal   :   log.fatal.bind(log)
+                    trace   :   function() {
+                        logHandler('trace', arguments);
+                    },
+                    debug   :   function() {
+                        logHandler('debug', arguments);
+                    },
+                    info    :   function() {
+                        logHandler('info', arguments);
+                    },
+                    warn    :   function() {
+                        logHandler('warn', arguments);
+                    },
+                    error   :   function() {
+                        logHandler('error', arguments);
+                    },
+                    fatal   :   function() {
+                        logHandler('fatal', arguments);
+                    }
                 };
             }
         };
