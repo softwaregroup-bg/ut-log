65d85d488e5618129eaa7e7b0bca9929cc0d8c65
stamen.peev <stamen.peev@softwaregroup-bg.com>
2015-05-29 10:56:50 +0300

    handle sentry initialization and operational exceptions

--- sentryStream.js
+++ sentryStream.js
@@ -7,28 +7,39 @@ var raven = require('raven');
 // settings.logger (string) optional (default: 'root') - if the sentry logger should have an unique identity. i.e. 'impl-name'
 function SentryStream(settings) {
     stream.Writable.call(this, {objectMode: true});
-    this.raven = new raven.Client(settings.dsn, {
-        logger: settings.logger || 'root'
-    });
-    if (settings.patchGlobal) {
-        this.raven.patchGlobal(function() {
-            console.log('Sentry: Uncaught exception occured...');
-            //process.exit(1);
+    try {
+        this.raven = new raven.Client(settings.dsn, {
+            logger: settings.logger || 'root',
+            dataCallback: function(data) {
+                // modify data if needed
+                return data;
+            }
         });
+        if (settings.patchGlobal) {
+            this.raven.patchGlobal(function() {
+                console.log('Sentry: Uncaught exception occured...');
+                //process.exit(1);
+            });
+        }
+        this.raven.on('logged', function() {
+            console.log('A message has been logged to Sentry');
+        });
+        this.raven.on('error', function(e) {
+            console.error('Sentry error : ', e.message);
+            // console.dir(e);
+        })
+    } catch (e) {
+        console.error(e)
+        this.raven = null;
     }
-    this.raven.on('logged', function() {
-        console.log('A message has been logged to Sentry');
-    });
-    this.raven.on('error', function(e) {
-        console.log('Sentry error : ');
-        console.dir(e);
-    })
 }
 
 util.inherits(SentryStream, stream.Writable);
 
 SentryStream.prototype._write = function(logMessage, encoding, done) {
-    if (logMessage.jsException) {
+    if(!this.raven){
+        //
+    } else if (logMessage.jsException) {
         this.raven.captureError(logMessage.jsException);
     } else {
         if (typeof logMessage == 'string' && logMessage.indexOf('jsException') !== -1) { // error already sent through winston logger
