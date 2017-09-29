c2839d46b29e4797024d325f4aa0e0c956a468d7
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2017-09-29 18:47:08 +1000

    fix: handle MTU

--- udpStream.js
+++ udpStream.js
@@ -8,6 +8,7 @@ function UdpStream(config) {
     this.socket = udp.createSocket(config.type || 'udp4');
     this.host = config.host;
     this.port = config.port;
+    this.mtu = config.mtu || 1400;
 }
 
 util.inherits(UdpStream, stream.Writable);
@@ -16,7 +17,20 @@ UdpStream.prototype._write = function(message, encoding, done) {
     if (typeof message === 'string') {
         message = new Buffer(message, encoding);
     }
-    this.socket.send(message, 0, message.length, this.port, this.host, done);
+    let sendFrame = (start, length) => {
+        if (start + length >= message.length) {
+            this.socket.send(message, start, message.length - start, this.port, this.host, done);
+        } else {
+            this.socket.send(message, start, length, this.port, this.host, err => {
+                if (err) {
+                    done(err);
+                } else {
+                    setImmediate(() => sendFrame(start + length, length));
+                }
+            });
+        }
+    };
+    sendFrame(0, this.mtu);
 };
 
 UdpStream.prototype.end = function(chunk, encoding, cb) {
