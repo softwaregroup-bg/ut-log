4da1df507529ed4e4e145ecb5a5aa5ed12bb074d
elin.angelov <elin.angelov@softwaregroup-bg.com>
2017-11-08 16:51:20 +0200

    feat: append to specific file name, based on log prop

--- logRotateStream.js
+++ logRotateStream.js
@@ -2,15 +2,16 @@ var stream = require('readable-stream');
 var util = require('util');
 var logRotateStream = require('stream-file-archive');
 var path = require('path');
+var fs = require('fs');
 var utils = require('./utils');
 // config : file, size, keep, compress
 // refer to: https://www.npmjs.com/package/logrotate-stream
 function LogRotate(config) {
-    var logDir = utils.createLogDir(config.workDir);
+    this.logDir = utils.createLogDir(config.workDir);
     stream.Transform.call(this);
     this.pipe(logRotateStream({
-        path: path.resolve(logDir, config.path || 'ut5-%Y-%m-%d.log'),  // Write logs rotated by the day
-        symlink: path.resolve(logDir, config.symlink || 'ut5.log'),    // Maintain a symlink called ut5.log
+        path: path.resolve(this.logDir, config.path || 'ut5-%Y-%m-%d.log'),  // Write logs rotated by the day
+        symlink: path.resolve(this.logDir, config.symlink || 'ut5.log'),    // Maintain a symlink called ut5.log
         compress: config.compress || false
     }));
 }
@@ -18,6 +19,14 @@ function LogRotate(config) {
 util.inherits(LogRotate, stream.Transform);
 
 LogRotate.prototype._transform = function(data, encoding, callback) {
+    var d;
+    try {
+        d = JSON.parse(data.toString());
+    } catch (e) {}
+    if (d && d.log) {
+        var logName = path.join(this.logDir, `${d.log}.log`);
+        fs.appendFile(logName, data, () => (true));
+    }
     this.push(data);
     callback();
 };
