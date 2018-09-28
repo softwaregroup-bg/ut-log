9612535c95483d39aa191700ec7640a05b329937
kalinkrustev <kalin.krustev@softwaregroup-bg.com>
2018-09-28 12:12:51 +0300

    fix: improve fluentd support

--- fluentdStream.js
+++ fluentdStream.js
@@ -1,16 +1,17 @@
 const logger = require('fluent-logger');
 const stream = require('readable-stream');
 const util = require('util');
+const fromDate = logger.EventTime.fromDate;
 
 function FluentdStream(config) {
     config = config || {};
     stream.Writable.call(this, {objectMode: true});
 
-    this.sender = logger.createFluentSender('ut', {
+    this.sender = logger.createFluentSender(config.prefix || 'ut', {
         host: 'localhost',
         port: 24224,
         timeout: 3.0,
-        reconnectInterval: 600000, // 10 minutes
+        reconnectInterval: 5000,
         ...config
     });
 }
@@ -20,8 +21,12 @@ util.inherits(FluentdStream, stream.Writable);
 FluentdStream.prototype._write = function(message, encoding, done) {
     if (typeof message === 'string') {
         message = Buffer.from(message, encoding);
+    } else {
+        message = Object.assign({}, message);
+        message['@meta'] = message.$meta;
+        delete message.$meta;
     }
-    this.sender.emit(message, done);
+    this.sender.emit(message.context, message, fromDate(message.time), done);
 };
 
 FluentdStream.prototype.end = function(chunk, encoding, cb) {
